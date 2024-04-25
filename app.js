a5d3a45c96b33260eb3a57c164f0a0ff07f460a5
I am [Px] <12368182+iamrossatopx@users.noreply.github.com>
2024-04-25 19:48:15 -0400

    adicionado paginas ainda em branco e funcoes dos botoes

--- app.js
+++ app.js
@@ -3,4 +3,26 @@ if (window.visualViewport.width < window.visualViewport.height) {
     document.getElementById("nav").style.justifyContent = "space-around";
 }
 
-window.onresize = function(){ location.reload(); }
+window.onresize = function() {
+    location.reload();
+}
+
+function initialPage() {
+    location.href = "index.html";
+}
+
+function loadFormacoes() {
+    location.href = "formacoes.html";
+}
+
+function loadHobbies() {
+    location.href = "hobbies.html";
+}
+
+function loadExps() {
+    location.href = "experiencias.html";
+}
+
+function contactme() {
+    location.href = "contactme.html";
+}
